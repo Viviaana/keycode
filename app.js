a43e1c3a7ae340156f70d6a1eb917bbd0296173e
Katie Ingham <kjingham91@gmail.com>
2020-02-18 17:29:04 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+function keycode(){
+    let key = event.key;
+    let code = event.code;
+    let which = event.which;
+    console.log(key);
+    console.log(code);
+    console.log(which);
+    }
