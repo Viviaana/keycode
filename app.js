3f333824578fdd24dfb650db84491998eb28f788
Katie Ingham <kjingham91@gmail.com>
2020-02-19 10:27:43 +0000

    got code to work

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-function keycode(){
-    let key = event.key;
-    let code = event.code;
-    let which = event.which;
-    console.log(key);
-    console.log(code);
-    console.log(which);
-    }
+// function keycode(){
+//     let key = event.key;
+//     let code = event.code;
+//     let which = event.which;
+//     console.log(key);
+//     console.log(code);
+//     console.log(which);
+//     }
+
+const body = document.querySelector("body");
+const key = document.getElementById("key");
+const code = document.getElementById("code");
+const which = document.getElementById("which");
+
+body.addEventListener("keydown", event =>{
+    key.textContent = event.key;
+});
+
+body.addEventListener("keydown", event => {
+    code.textContent = event.code;
+});
+
+body.addEventListener("keydown", event => {
+    which.textContent = event.which;
+});
